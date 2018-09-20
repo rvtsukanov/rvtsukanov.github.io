f074448d2ff182ac6c7d65e55b75b23405c94e10
Roman <rvtsukanov@edu.hse.ru>
2018-09-20 10:23:21 +0300

    almost done

--- script.js
+++ script.js
@@ -1,15 +1,79 @@
+ymaps.ready(init);
+var myMap;
+
+
+function init(){
+    myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 7
+    });
+}
+
+var http = new XMLHttpRequest();
+var parser = new DOMParser();
+var points = [];
+
+
+
+var added = 0;
+var send_request = function (msg) {
+    console.log('Sended: ', msg);
+    http.open('GET', ' https://geocode-maps.yandex.ru/1.x/?geocode=' + msg, false);
+    http.send(null);
+    // console.log(http.responseText);
+    xmlDoc = parser.parseFromString(http.responseText, 'text/xml');
+    console.log(xmlDoc.getElementsByTagName('pos')[0].textContent);
+    points.push(xmlDoc.getElementsByTagName('pos')[0].textContent);
+}
+
+var field = document.getElementById('field');
+console.log(field);
+
+field.oninput = function () {
+    console.log(field.value.split(' - '));
+    console.log(added);
+    var req = field.value.split(' - ');
+
+    if(req.length === 1) {added = 0; points = [];}
+    else if (added === req.length-2){send_request(req[req.length - 2]); added++};
+}
+field.onchange = function () {
+    var req = field.value.split(' - ');
+    if(req[req.length - 1] !== '') {
+        send_request(req[req.length - 1]);
+    }
+}
+
+
 $(document).ready(function(){
-    $('#lilic').css('color', 'black');
-    var chosen = 'lic';
+
+    function main_frame(){
+        var content = document.getElementsByClassName('inside')[0].getElementsByTagName('*');
+        var list_of_content = [];
+
+        for(var i = 0; i < content.length; i++){
+            list_of_content.push(content[i].id);
+        }
+        this.active_tab = content[0].id;
+        this.len = content.length;
+        this.list_of_content = content;
+    }
+
+    let frame = new main_frame();
+    document.getElementById(frame.active_tab).style.color = 'black';
 
 
     $('.container li').hover(function(){
         $(this).css('color', 'black');
     }, function(){
-        $(this).css('color', 'white')
+        if(this.id !== frame.active_tab) {
+            $(this).css('color', 'white');
+        }
     });
 
+
     $('#lilic').click(function() {
+        frame.active_tab = 'lilic';
         $('div.main-table').css('display', 'flex');
         $('#map').css('display', 'none');
         $('div.order').css('display', 'none');
@@ -19,6 +83,7 @@ $(document).ready(function(){
     });
 
     $('#limap').click(function() {
+        frame.active_tab = 'limap';
         $('div.main-table').css('display', 'none');
         $('#map').css('display', 'flex');
         $('div.order').css('display', 'none');
@@ -28,6 +93,7 @@ $(document).ready(function(){
     });
 
     $('#liord').click(function() {
+        frame.active_tab = 'liord';
         $('div.main-table').css('display', 'none');
         $('#map').css('display', 'none');
         $('div.order').css('display', 'flex');
